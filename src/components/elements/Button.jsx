26efa8cad7f4bb428b2acd46d9b6497a691169e2
Marwin Buena <marwinfb@gmail.com>
2026-03-26 21:49:50 +0800

    add type attribute to button

--- src/components/elements/Button.jsx
+++ src/components/elements/Button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Button() {
   return (
-    <button className="font-bold text-lg leading-[1.3] py-[16.5px] px-[106.5px] rounded-full w-full cursor-pointer text-white bg-primary-navy-950 hover:bg-linear-to-b from-[#7755FF] to-[#2F2CE9]">
+    <button type="button" className="font-bold text-lg leading-[1.3] py-[16.5px] px-[106.5px] rounded-full w-full cursor-pointer text-white bg-primary-navy-950 hover:bg-linear-to-b from-[#7755FF] to-[#2F2CE9]">
       Continue
     </button>
   );
